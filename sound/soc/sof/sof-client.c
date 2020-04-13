b650a1bba4411e6ce91cdd07b46035fe50976588
Fred Oh <fred.oh@linux.intel.com>
2020-04-13 11:24:13 -0700

    ASoC: SOF: add client APIs for IPC tx and accessing debugfs root
    Add a couple of client APIs for IPC tx and querying the SOF
    debugfs root entry.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Fred Oh <fred.oh@linux.intel.com>

--- sound/soc/sof/sof-client.c
+++ sound/soc/sof/sof-client.c
@@ -6,6 +6,7 @@
 //
 
 #include <linux/completion.h>
+#include <linux/debugfs.h>
 #include <linux/device.h>
 #include <linux/errno.h>
 #include <linux/jiffies.h>
@@ -70,6 +71,21 @@ int sof_client_dev_register(struct snd_sof_dev *sdev,
 	return 0;
 }
 EXPORT_SYMBOL_NS_GPL(sof_client_dev_register, SND_SOC_SOF_CLIENT);
+
+int sof_client_ipc_tx_message(struct sof_client_dev *cdev, u32 header,
+			      void *msg_data, size_t msg_bytes,
+			      void *reply_data, size_t reply_bytes)
+{
+	return sof_ipc_tx_message(cdev->sdev->ipc, header, msg_data, msg_bytes,
+				  reply_data, reply_bytes);
+}
+EXPORT_SYMBOL_NS_GPL(sof_client_ipc_tx_message, SND_SOC_SOF_CLIENT);
+
+struct dentry *sof_client_get_debugfs_root(struct sof_client_dev *cdev)
+{
+	return cdev->sdev->debugfs_root;
+}
+EXPORT_SYMBOL_NS_GPL(sof_client_get_debugfs_root, SND_SOC_SOF_CLIENT);
 
 MODULE_AUTHOR("Ranjani Sridharan <ranjani.sridharan@linux.intel.com>");
 MODULE_LICENSE("GPL v2");
