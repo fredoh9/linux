04372dd94b603c432502d351d8e3bb8167077454
Fred Oh <fred.oh@linux.intel.com>
2020-04-13 11:24:13 -0700

    ASoC: SOF: add client APIs for IPC tx and accessing debugfs root
    Add a couple of client APIs for IPC tx and querying the SOF
    debugfs root entry.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Fred Oh <fred.oh@linux.intel.com>

--- sound/soc/sof/sof-client.h
+++ sound/soc/sof/sof-client.h
@@ -4,6 +4,7 @@
 #define __SOUND_SOC_SOF_CLIENT_H
 
 #include <linux/completion.h>
+#include <linux/debugfs.h>
 #include <linux/device.h>
 #include <linux/device/driver.h>
 #include <linux/kernel.h>
@@ -61,6 +62,12 @@ static inline void sof_client_dev_unregister(struct sof_client_dev *cdev)
 {
 	virtbus_unregister_device(&cdev->vdev);
 }
+
+int sof_client_ipc_tx_message(struct sof_client_dev *cdev, u32 header,
+			      void *msg_data, size_t msg_bytes,
+			      void *reply_data, size_t reply_bytes);
+
+struct dentry *sof_client_get_debugfs_root(struct sof_client_dev *cdev);
 
 /**
  * module_sof_client_driver() - Helper macro for registering an SOF Client
